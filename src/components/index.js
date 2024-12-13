87f59aff61727df8cb71fe81ed5ea294611feb6e
snippetarnab <arnabvivo2020@gmail.com>
2024-12-14 00:52:05 +0530

    Pages update

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,21 @@ import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Input from "./Input.jsx";
 import Button from "./Button.jsx";
+import Select from "./Select.jsx";
+import RTE from "./RTE.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard.jsx";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input, Button };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Select,
+  RTE,
+  PostForm,
+  PostCard,
+};
