724791067227bf3603e21fa3a2711d81bd4c3fad
snippetarnab <arnabvivo2020@gmail.com>
2025-04-12 23:09:37 +0530

    Chnged the Home UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
 import { PostCard, Container } from "../components";
 
@@ -23,30 +23,37 @@ function Home() {
               {/* <h1 className="text-2xl font-bold hover:text-gray-500">
                 Login to read posts
               </h1> */}
-              <div class="max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+              <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden">
                 <img
-                  class="w-full h-48 object-cover"
+                  className="w-full h-48 object-cover"
                   src="https://avatars.githubusercontent.com/u/160257879?v=4"
                   alt="Profile picture"
                 />
-                <div class="p-4">
-                  <h2 class="text-lg font-semibold text-gray-800">Arnab</h2>
-                  <p class="text-sm text-gray-500">Developer / Designer</p>
-                  <div class="flex items-center justify-center space-x-4 mt-4">
-                    <a href="#" class="text-blue-500 hover:text-blue-700">
-                      <i class="fab fa-facebook"></i>
+                <div className="p-4">
+                  <h2 className="text-lg font-semibold text-gray-800 font-mono">
+                    Arnab
+                  </h2>
+                  <p className="text-sm text-gray-500 font-serif">
+                    This is a website for pet lover.
+                  </p>
+                  <div className="flex items-center justify-center space-x-4 mt-4">
+                    <a
+                      href="https://github.com/snippetarnab"
+                      className="text-black-500 hover:text-blue-700"
+                    >
+                      <i className="fab fa-github" />
                     </a>
                     <a
                       href="https://x.com/imArnabParua?t=yAnk60IAsBwevSB9gliCjQ&s=09"
-                      class="text-blue-400 hover:text-blue-600"
+                      className="text-black-400 hover:text-blue-600"
                     >
-                      <i class="fab fa-twitter"></i>
+                      <i className="fab fa-x-twitter" />
                     </a>
                     <a
                       href="https://www.instagram.com/_i.arnab_?igsh=Nm1kNHcxdnFxOTV2"
-                      class="text-pink-500 hover:text-pink-700"
+                      className="text-black-500 hover:text-pink-700"
                     >
-                      <i class="fab fa-instagram"></i>
+                      <i className="fab fa-instagram" />
                     </a>
                   </div>
                 </div>
